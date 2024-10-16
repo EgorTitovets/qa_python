33a07cd83ded299a7ad4b141cf345e6661fa0962
Egor Titovets <pryanik0992@gmail.com>
2024-10-16 16:37:06 +0500

    The first version of the unit tests. 9 basic tests for BooksCollector and added a description of the tests to the README file.

--- tests.py
+++ tests.py
@@ -1,24 +1,72 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+    @pytest.fixture
+    def collector(self):
+        return BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+    def test_add_new_book_duplicate_books_does_not_added(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.add_new_book("Один Дома")
+        assert len(collector.books_genre) == 1
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    @pytest.mark.parametrize("book_name, genre", [
+        ("Один Дома", "Комедии"),
+        ("Король Лев", "Мультфильмы"),
+        ("Аватар", "Фантастика")
+    ])
+    def test_set_book_genre_valid_genre_assigned_genre(self, collector, book_name, genre):
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+        assert collector.books_genre[book_name] == genre
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    @pytest.mark.parametrize("book_name, genre", [
+        ("Один Дома", "Комедии"),
+        ("Король Лев", "Мультфильмы"),
+        ("Аватар", "Фантастика")
+    ])
+    def test_get_book_genre_existing_book_genre_returns(self, collector, book_name, genre):
+        collector.add_new_book(book_name)
+        collector.set_book_genre(book_name, genre)
+        assert collector.get_book_genre(book_name) == genre
+
+    def test_get_books_with_specific_genre_existing_genre_books_return(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.set_book_genre("Один Дома", "Комедии")
+        collector.add_new_book("Король Лев")
+        collector.set_book_genre("Король Лев", "Мультфильмы")
+        assert "Король Лев" in collector.get_books_with_specific_genre("Мультфильмы")
+
+    def test_get_books_genre_correct_dictionary_returns(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.set_book_genre("Один Дома", "Комедии")
+        collector.add_new_book("Король Лев")
+        collector.set_book_genre("Король Лев", "Мультфильмы")
+
+        expected_dictionary = {"Один Дома": "Комедии", "Король Лев": "Мультфильмы"}
+        assert collector.get_books_genre() == expected_dictionary
+
+    def test_get_books_for_children_no_age_rating_books_without_age_rating_return(self, collector):
+        collector.add_new_book("Король Лев")
+        collector.set_book_genre("Король Лев", "Мультфильмы")
+        assert "Король Лев" in collector.get_books_for_children()
+
+    def test_add_book_in_favorites_add_one_book_book_added_in_favorites(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.add_book_in_favorites("Один Дома")
+        assert "Один Дома" in collector.favorites
+
+    def test_delete_book_from_favorites_remove_real_book_book_removed(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.add_book_in_favorites("Один Дома")
+        collector.delete_book_from_favorites("Один Дома")
+        assert "Один Дома" not in collector.favorites
+
+    def test_get_list_of_favorites_books_with_real_books_list_favorites_returns(self, collector):
+        collector.add_new_book("Один Дома")
+        collector.add_book_in_favorites("Один Дома")
+        assert collector.get_list_of_favorites_books() == ["Один Дома"]
